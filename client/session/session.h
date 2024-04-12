0b6b3b6ea0dd4835b9d09d3cd339beda7c3455d6
Soamane <soamane@mail.ru>
2024-04-13 01:27:43 +0300

    Реализован класс сессии

---
+++ client/session/session.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef SESSION_H
+#define SESSION_H
+
+#include <boost/asio.hpp>
+
+class Session
+{
+public:
+	Session(boost::asio::ip::tcp::socket& socket);
+
+	void run();
+
+private:
+	boost::asio::ip::tcp::socket socket;
+};
+
+#endif // !SESSION_H
