1647c58a5262f20e8789e73181004789703d2f4f
Soamane <soamane@mail.ru>
2024-04-12 18:59:41 +0300

    Реализован конструктор и метод подключения к серверу

--- client/connector/connector.h
+++ client/connector/connector.h
@@ -2,14 +2,21 @@
 #ifndef CONNECTOR_H
 #define CONNECTOR_H
 
+#include <string_view>
 #include <boost/asio.hpp>
 
 class Connector
 {
 public:
+	Connector(boost::asio::io_service& ioService);
+	void Connect(std::string_view address, std::string_view port);
 
 private:
+	void CreateSession();
 
+private:
+	boost::asio::ip::tcp::socket socket;
+	boost::asio::ip::tcp::resolver resolver;
 };
 
 #endif // !CONNECTOR_H
