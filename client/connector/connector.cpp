c32938c57f11d3dd60e7fc9c99e4028b2dd6df6a
soamane <somane@mail.ru>
2024-04-12 18:59:41 +0300

    Реализован конструктор и метод подключения к серверу

--- client/connector/connector.cpp
+++ client/connector/connector.cpp
@@ -1,1 +1,21 @@
 #include "connector.h"
+
+Connector::Connector(boost::asio::io_service& ioService)
+	: socket(ioService), resolver(ioService) {
+}
+
+void Connector::Connect(std::string_view address, std::string_view port) {
+	boost::asio::ip::tcp::resolver::query query(address.data(), port.data());
+	boost::asio::ip::tcp::resolver::iterator iterator = this->resolver.resolve(query);
+
+	boost::system::error_code errorCode{ };
+	boost::asio::connect(this->socket, iterator, errorCode);
+	if (errorCode) {
+		// TODO: exception logic
+	}
+
+	this->CreateSession();
+}
+
+void Connector::CreateSession() {
+}
