5a9c64a5d1638142d36da89588118fdcaaa1135f
Soamane <soamane@mail.ru>
2024-04-14 22:17:50 +0300

    Описан пример получения пакета ( сообщения ) от клиента

--- server/session/session.cpp
+++ server/session/session.cpp
@@ -9,12 +9,8 @@ Session::Session(boost::asio::ip::tcp::socket& socket)
 void Session::run() {
 	auto self(shared_from_this());
 
-	self->packetHandler->sendMessage("test_packet_1", [self](bool status) {
-		if (status) {
-			self->packetHandler->recvMessage([self](const std::string& callback) {
-				std::cout << callback.c_str();
-			});
-		}
+	self->packetHandler->recvMessage([self](const std::string& message) {
+		std::cout << message.c_str();
 	});
 
 }
