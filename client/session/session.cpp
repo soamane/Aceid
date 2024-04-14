9663c3a6c157809bd8186cd50403957cd2aa112d
Soamane <soamane@mail.ru>
2024-04-14 14:51:42 +0300

    Добавлены тестовые функции получения сообщения и буфера от сервера

--- client/session/session.cpp
+++ client/session/session.cpp
@@ -4,5 +4,7 @@ Session::Session(boost::asio::ip::tcp::socket& socket)
 	: socket(std::move(socket)), packetHandler(std::make_unique<PacketHandler>(this->socket)) { }
 
 void Session::run() {
-
+	this->packetHandler->recvMessage();
+	this->packetHandler->recvBuffer();
+	Sleep(-1);
 }
