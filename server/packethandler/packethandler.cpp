7507068d33973262a8dfcf4af80a59f957933129
Soamane <soamane@mail.ru>
2024-04-14 21:50:11 +0300

    Добавлен метод асинхронного чтения пакета от клиента

--- server/packethandler/packethandler.cpp
+++ server/packethandler/packethandler.cpp
@@ -12,9 +12,7 @@ void PacketHandler::sendMessage(const std::string& message, std::function<void(b
         packet.data = std::vector<char>(message.begin(), message.end());
     }
 
-    this->sendPacket(packet, [callback](bool status) {
-        status ? callback(true) : callback(false);
-    });
+    this->sendPacket(packet, callback);
 }
 
 void PacketHandler::sendBuffer(const std::vector<char>& buffer, std::function<void(bool)> callback) {
@@ -24,34 +22,62 @@ void PacketHandler::sendBuffer(const std::vector<char>& buffer, std::function<vo
         packet.data = buffer;
     }
 
-    this->sendPacket(packet, [callback](bool status) {
-        status ? callback(true) : callback(false);
-    });
+    this->sendPacket(packet, callback);
+}
+
+void PacketHandler::recvMessage(std::function<void(const std::string&)> callback) {
+    Packet packet;
+    {
+        this->recvPacket(packet, callback);
+    }
 }
 
 void PacketHandler::sendPacket(const Packet& packet, std::function<void(bool)> callback) {
     std::shared_ptr<Packet> packetPointer = std::make_shared<Packet>(packet);
 
     auto self(shared_from_this());
-    boost::asio::async_write(self->socket, boost::asio::buffer(&packet.size, sizeof(packet.size)), [self, packetPointer, callback](boost::system::error_code errorCode, std::size_t) {
+    boost::asio::async_write(self->socket, boost::asio::buffer(&packetPointer->size, sizeof(packetPointer->size)), [self, packetPointer, callback](boost::system::error_code errorCode, std::size_t) {
         if (!errorCode) {
             boost::asio::async_write(self->socket, boost::asio::buffer(packetPointer->data), [self, packetPointer, callback](boost::system::error_code errorCode, std::size_t) {
                 if (!errorCode) {
-                    // TODO: the log of successful sending
                     callback(true);
                 }
                 else {
-                    // TODO: the log of the unsuccessful sending of the data
                     callback(false);
                 }
             });
         }
         else {
-            // TODO: the log of the unsuccessful sending of the size
             callback(false);
         }
     });
 }
+
+void PacketHandler::recvPacket(const Packet& packet, std::function<void(const std::string&)> callback) {
+    std::shared_ptr<Packet> packetPointer = std::make_shared<Packet>(packet);
+
+    auto self(shared_from_this());
+    boost::asio::async_read(self->socket, boost::asio::buffer(&packetPointer->size, sizeof(packetPointer->size)), [self, packetPointer, callback](boost::system::error_code errorCode, std::size_t b) {
+        if (!errorCode) {
+
+            packetPointer->data.resize(packetPointer->size);
+            boost::asio::async_read(self->socket, boost::asio::buffer(packetPointer->data), [self, packetPointer, callback](boost::system::error_code errorCode, std::size_t b) {
+                if (!errorCode) {
+                    std::string result(packetPointer->data.begin(), packetPointer->data.end());
+                    callback(result);
+
+                }
+                else {
+                    callback(std::string());
+                }
+            });
+        }
+        else {
+            callback(std::string());
+        }
+    });
+}
+
 
 
 
