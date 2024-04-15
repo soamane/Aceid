59f49fae42525e89955996f915fa6f33f30660b6
soamane <somane@mail.ru>
2024-04-15 23:57:45 +0300

    Небольшие изменения

--- client/session/session.cpp
+++ client/session/session.cpp
@@ -4,7 +4,7 @@ Session::Session(boost::asio::ip::tcp::socket& socket)
 	: socket(std::move(socket)), packetHandler(std::make_unique<PacketHandler>(this->socket)) { }
 
 void Session::run() {
-	if (!this->packetHandler->sendMessage("hello, server!")) {
+	if (!this->packetHandler->sendMessage("test_message_#1")) {
 		// TODO: error log
 		return;
 	}
