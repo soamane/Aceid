a73b918056b6c4bcabf3ba8e03a75f7555a5bd12
Soamane <soamane@mail.ru>
2024-04-15 18:29:18 +0300

    Добавлен класс обработчика файлов json

---
+++ server/jsonhandler/jsonhandler.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef JSON_HANDLER_H
+#define JSON_HANDLER_H
+
+#include <rapidjson.h>
+
+class JsonHandler 
+{
+public:
+	static JsonHandler* getInstance();
+
+private:
+
+};
+
+#endif // !JSON_HANDLER_H
