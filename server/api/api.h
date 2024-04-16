47c93d7c707b5d7ea2e388e04545fc8145193f49
soamane <somane@mail.ru>
2024-04-16 12:14:36 +0300

    Добавлен класс для взаимодействия с API форума

---
+++ server/api/api.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef API_H
+#define API_H
+
+#include "../jsonwrapper/jsonwrapper.h"
+#include "../curlwrapper/curlwrapper.h"
+
+class API
+{
+public:
+
+private:
+};
+
+#endif // !API_H
