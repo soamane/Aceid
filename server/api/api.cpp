f58d88794c71e6019aeef625526e589b8a71525d
soamane <somane@mail.ru>
2024-04-16 13:16:30 +0300

    Добавлена функция проверки лицензии пользователя

--- server/api/api.cpp
+++ server/api/api.cpp
@@ -73,3 +73,33 @@ bool API::checkUserHwid() {
 
 	return jsonWrapper->isErrorField(response);
 }
+
+bool API::checkUserLicense() {
+	static JsonWrapper* jsonWrapper = JsonWrapper::getInstance();
+	if (jsonWrapper == nullptr) {
+		// TODO: exception log
+		return false;
+	}
+
+	const std::string jsonString = jsonWrapper->createJsonString
+	(
+		{ { "action", "license" },
+		  { "type", "get" }
+		},
+		{
+			{ "username", this->data.username },
+			{ "password", this->data.password }
+		}
+	);
+
+	static CurlWrapper* curlWrapper = CurlWrapper::getInstance();
+	if (curlWrapper == nullptr) {
+		// TODO: exception log
+		return false;
+	}
+
+	boost::format formatString = boost::format("%1%data=%2%") % this->source % jsonString;
+	const std::string response = curlWrapper->performRequest(RequestType::eRT_HTTPS, formatString.str(), nullptr);
+
+	return jsonWrapper->isErrorField(response);
+}
