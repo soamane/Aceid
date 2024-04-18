e0a215725f984aebf4e6fc670a15616a26608c33
soamane <somane@mail.ru>
2024-04-18 21:48:29 +0300

    Реализован класс CurlWrapper

--- client/curlwrapper/curlwrapper.h
+++ client/curlwrapper/curlwrapper.h
@@ -2,9 +2,28 @@
 #ifndef CURL_WRAPPER_H
 #define CURL_WRAPPER_H
 
+#include <string>
+#include <initializer_list>
+
+#include <curl/curl.h>
+
+enum RequestType
+{
+	eRT_HTTP,
+	eRT_HTTPS
+};
+
 class CurlWrapper
 {
+public:
+	static CurlWrapper* getInstance();
 
+	const curl_slist* addHeaders(std::initializer_list<std::string> headers);
+	const std::string performRequest(RequestType type, const std::string& source, const curl_slist* headers);
+
+private:
+	CurlWrapper();
+	static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* buffer);
 };
 
 #endif // !CURL_WRAPPER_H
