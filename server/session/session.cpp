76986845cefc4a72bffa3a686ad64ca7fc212f01
soamane <somane@mail.ru>
2024-04-18 23:17:25 +0300

    Пример обработки данных клиента

--- server/session/session.cpp
+++ server/session/session.cpp
@@ -1,4 +1,8 @@
 #include "session.h"
+#include "../api/api.h"
+
+#include <iostream>
+
 
 Session::Session(boost::asio::ip::tcp::socket& socket)
 	: socket(std::move(socket)), packetHandler(std::make_shared<PacketHandler>(this->socket)) {
@@ -8,7 +12,8 @@ void Session::run() {
 	auto self(shared_from_this());
 
 	self->packetHandler->recvMessage([self](const std::string& message) {
-
+		API api(message);
+		std::cout << api.isAuthorized();
 	});
 
 }
