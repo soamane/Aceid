81e7539a873e7eb193d9e5e4d8b16946b133247c
Soamane <soamane@mail.ru>
2024-04-18 23:16:02 +0300

    Пример запроса на сервер

--- client/session/session.cpp
+++ client/session/session.cpp
@@ -1,10 +1,21 @@
 #include "session.h"
 
+#include "../api/api.h"
+
 Session::Session(boost::asio::ip::tcp::socket& socket) 
 	: socket(std::move(socket)), packetHandler(std::make_unique<PacketHandler>(this->socket)) { }
 
 void Session::run() {
-	if (!this->packetHandler->sendMessage("test_message_#1")) {
+	std::unique_ptr<AuthData> data = std::make_unique<AuthData>();
+	std::unique_ptr<API> api = std::make_unique<API>(data.get());
+
+	data->username = "username_value";
+	data->password = "password_value";
+	data->hwid = "hwid_value";
+	data->token = api->getSessionToken();
+
+	std::string request = api->convertAuthDataToJson();
+	if (!this->packetHandler->sendMessage(request)) {
 		// TODO: error log
 		return;
 	}
