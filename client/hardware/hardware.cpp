e36da19bb7416d106bba6445419dafb1aee57ef3
Soamane <soamane@mail.ru>
2024-04-23 22:45:07 +0300

    Добавлена система получения HardwareID

---
+++ client/hardware/hardware.cpp
@@ -0,0 +1,51 @@
+#include "hardware.h"
+
+#include <intrin.h>
+#include <sstream>
+
+#include <d3d9.h>
+#pragma comment(lib, "d3d9.lib")
+
+std::string Hardware::getHardwareId() {
+    std::string cpuInfo = getCPUInfo();
+    if (cpuInfo.empty()) {
+        return std::string();
+    }
+
+    std::string videoCard = getGPUInfo();
+    if (videoCard.empty()) {
+        return std::string();
+    }
+
+    return cpuInfo + videoCard;
+}
+
+std::string Hardware::getCPUInfo() {
+    std::uint32_t regs[4]{ };
+    __cpuid(reinterpret_cast<int*>(regs), 0);
+
+    std::ostringstream oss;
+    oss << regs[0] << regs[1] << regs[2] << regs[3];
+
+    return oss.str();
+}
+
+std::string Hardware::getGPUInfo() {
+    IDirect3D9* directDevice = Direct3DCreate9(D3D_SDK_VERSION);
+    if (directDevice == nullptr) {
+        return std::string();
+    }
+
+    D3DADAPTER_IDENTIFIER9 adapterIdentifier;
+    if (FAILED(directDevice->GetAdapterIdentifier(0, 0, &adapterIdentifier))) {
+        return std::string();
+    }
+
+    std::ostringstream oss;
+    oss << adapterIdentifier.Description;
+    oss << adapterIdentifier.DeviceIdentifier.Data1
+        << adapterIdentifier.DeviceIdentifier.Data2
+        << adapterIdentifier.DeviceIdentifier.Data3;
+
+    return oss.str();
+}
