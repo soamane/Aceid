6593c0066121ffc529a5a99ad697ffc22c2d9354
soamane <somane@mail.ru>
2024-04-26 16:58:57 +0300

    Написано кастомное шифрование на основе base64

--- client/protect/dataencryption/dataencryption.cpp
+++ client/protect/dataencryption/dataencryption.cpp
@@ -9,3 +9,39 @@ std::string DataEncryption::encryptBase64(const std::string& str) {
 std::string DataEncryption::decryptBase64(const std::string& str) {
 	return base64::from_base64(str);
 }
+
+std::string DataEncryption::encryptMultiBase64(const std::string& str) {
+	std::string result;
+	for (auto& c : str) {
+		std::string encryptedChar = encryptBase64(std::string(1, c));
+		result += encryptedChar;
+	}
+
+	size_t pos = result.find('=');
+	while (pos != std::string::npos) {
+		result.replace(pos, 1, salt);
+		pos = result.find('=', pos + 1);
+	}
+
+	result = encryptBase64(result);
+	return result;
+}
+
+std::string DataEncryption::decryptMultiBase64(const std::string& str) {
+	std::string result;
+	std::string decryptedStr = decryptBase64(str);
+
+	size_t pos = decryptedStr.find(salt);
+	while (pos != std::string::npos) {
+		decryptedStr.replace(pos, salt.size(), "=");
+		pos = decryptedStr.find(salt, pos + 1);
+	}
+
+	for (size_t i = 0; i < decryptedStr.length(); i += 4) {
+		std::string chunk = decryptedStr.substr(i, 4);
+		result += base64::from_base64(chunk);
+	}
+
+	return result;
+}
+
