70f1d6c36192b525041e91d93412d4bb32e0621e
soamane <somane@mail.ru>
2024-04-29 15:00:49 +0300

    Написан более интерактивная логика сессии

--- client/client/session/session.cpp
+++ client/client/session/session.cpp
@@ -5,23 +5,27 @@
 
 #include <iostream>
 
-Session::Session(boost::asio::ip::tcp::socket& socket) 
+Session::Session(boost::asio::ip::tcp::socket& socket)
 	: m_socket(std::move(socket)), m_packetHandler(std::make_unique<PacketHandler>(m_socket)) { }
 
 void Session::run() {
 	std::unique_ptr<AuthData> authData = std::make_unique<AuthData>();
 	std::unique_ptr<API> api = std::make_unique<API>(authData.get());
 
-	authData->username = "admin";
-	authData->password = "aceid";
+	std::cout << "username: ";
+	std::cin >> authData->username;
+
+	std::cout << "password: ";
+	std::cin >> authData->password;
 
 	authData->hwid = Hardware::getHardwareId();
 	authData->token = api->getSessionToken();
 
-	std::string convertedAuthData = api->convertAuthDataToJson();
-
+	const std::string convertedAuthData = api->convertAuthDataToJson();
 	m_packetHandler->sendMessage(convertedAuthData);
-	std::cout << m_packetHandler->recvMessage();
+
+	const std::string serverResponse = m_packetHandler->recvMessage();
+	std::cout << "server response: " << serverResponse << std::endl;
 
 	Sleep(-1);
 }
