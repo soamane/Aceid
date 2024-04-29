19726bd692ae25f450ce98581ea70ffc6c4fcdfd
soamane <somane@mail.ru>
2024-04-30 00:34:42 +0300

    Динамическое генерирование кей-кода для шифрование на двух сторонах

---
+++ server/app/logsystem/log/serverlog/serverlog.h
@@ -0,0 +1,27 @@
+#pragma once
+#ifndef SERVER_LOG_H
+#define SERVER_LOG_H
+
+#include <string>
+#include <fstream>
+
+#include "../log.h"
+
+/*
+
+    Class for working with server logs (errors, exceptions and more)
+
+*/
+
+class ServerLog : public Log
+{
+public:
+    // Log writing function
+    void write(const std::string& log) override;
+
+public:
+    std::ofstream m_file;
+    std::string m_fileName = "server.log";
+};
+
+#endif // !SERVER_LOG_H
