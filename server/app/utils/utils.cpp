cef938270b38040245aa4882d4d68dea9fdfa304
Soamane <soamane@mail.ru>
2024-05-01 01:29:33 +0300

    Добавлено логирование новый методов

--- server/app/utils/utils.cpp
+++ server/app/utils/utils.cpp
@@ -2,24 +2,31 @@
 
 #include <fstream>
 
+#include "../logsystem/logmanager/logmanager.h"
+
 const std::vector<char> Utils::convertFileToBytes(const std::string& path) {
 	std::ifstream file(path, std::ios::binary);
 	if (!file.is_open()) {
+		CREATE_EVENT_LOG("Failed to open target file")
 		return std::vector<char>();
 	}
 
 	std::vector<char> bytes((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 	file.close();
 
+	CREATE_EVENT_LOG("File converted to bytes array")
 	return bytes;
 }
 
 void Utils::createFileFromBytes(const std::string& path, const std::vector<char>& bytes) {
 	std::ofstream file(path, std::ios::binary);
 	if (!file.is_open()) {
+		CREATE_EVENT_LOG("Failed to open target file")
 		return;
 	}
 
 	file.write(bytes.data(), bytes.size());
 	file.close();
+
+	CREATE_EVENT_LOG("File converted from bytes array")
 }
