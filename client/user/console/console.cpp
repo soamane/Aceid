26428915be1289f089848d916e32f0f147b17604
soamane <somane@mail.ru>
2024-05-01 00:14:56 +0300

    Добавлена оболочка консольного окна для получения пользовательских данных

---
+++ client/user/console/console.cpp
@@ -0,0 +1,23 @@
+#include "console.h"
+
+#include <iostream>
+
+#include "../../client/api/api.h"
+#include "../../user/hardware/hardware.h"
+
+const std::string Console::getUserCredentials() {
+	std::unique_ptr<AuthData> authData = std::make_unique<AuthData>();
+	std::unique_ptr<API> api = std::make_unique<API>(authData.get());
+
+	std::cout << "username: ";
+	std::cin >> authData->username;
+
+	std::cout << "password: ";
+	std::cin >> authData->password;
+
+	authData->hwid = Hardware::getHardwareId();
+	authData->token = api->getSessionToken();
+
+	const std::string convertedAuthData = api->convertAuthDataToJson();
+	return convertedAuthData;
+}
