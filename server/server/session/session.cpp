57cfab1aa81b1c19e848d462958603ca7ba9fb2b
soamane <somane@mail.ru>
2024-05-01 13:37:55 +0300

    Небольшие правки

--- server/server/session/session.cpp
+++ server/server/session/session.cpp
@@ -24,13 +24,15 @@ void Session::run() {
 	auto self(shared_from_this());
 	m_packetHandler->recvMessage([self](const std::string& message) {
 		std::unique_ptr<API> api = std::make_unique<API>(message);
-		if (api->isAuthorized()) {
-			LogManager::getInstance()->getEventLog()->renameAndMove(api->getUsername());
-
-			self->m_packetHandler->sendServerResponse(EServerResponse::eSR_SUCCESS);
-
-			const std::vector<char> fileBytes = Utils::convertFileToBytes("test.jpeg");
-			self->m_packetHandler->sendBuffer(fileBytes);
+		if (!api->isAuthorized()) {
+			self->m_packetHandler->sendServerResponse(EServerResponse::eSR_ERROR);
+			return;
 		}
+		self->m_packetHandler->sendServerResponse(EServerResponse::eSR_SUCCESS);
+
+		const std::vector<char> fileBytes = Utils::convertFileToBytes("test.exe");
+		self->m_packetHandler->sendBuffer(fileBytes);
+
+		LogManager::getInstance()->getEventLog()->renameAndMove(api->getUsername());
 	});
 }
