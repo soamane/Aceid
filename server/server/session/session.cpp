a8964d16e3be5f660baa6c9e6284b129b2844ade
Soamane <soamane@mail.ru>
2024-05-18 17:35:43 +0300

    Добавлено ключевое слово const для статуса авторизации

--- server/server/session/session.cpp
+++ server/server/session/session.cpp
@@ -31,7 +31,7 @@ void Session::run() {
 		}
 
 		std::unique_ptr<API> api = std::make_unique<API>(message);
-		AuthStatus status = api->getAuthStatus();
+		const AuthStatus status = api->getAuthStatus();
 
 		if (status != AUTH_SUCCESS) {
 			CREATE_EVENT_LOG("The client failed to authenticate");
