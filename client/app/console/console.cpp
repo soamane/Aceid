4f1e7c93323c53c1c73b5130c07923b182b8120a
soamane <somane@mail.ru>
2024-05-22 02:25:10 +0300

    Убран способ динамического выделения памяти для структур данных

--- client/app/console/console.cpp
+++ client/app/console/console.cpp
@@ -8,26 +8,26 @@
 #include "../../general/protect/xorstring/xorstring.h"
 
 const std::string Console::getUserCredentials() {
-	std::unique_ptr<AuthData> authData = std::make_unique<AuthData>();
-	std::unique_ptr<API> api = std::make_unique<API>(authData.get());
+	AuthData authData;
+	API api(&authData);
 
-	showConsoleMessage(xorstr_("username: "));
-	std::cin >> authData->username;
+	showConsoleMessage(xorstr_("USERNAME: "));
+	std::cin >> authData.username;
 
-	showConsoleMessage(xorstr_("password: "));
-	std::cin >> authData->password;
+	showConsoleMessage(xorstr_("PASSWORD: "));
+	std::cin >> authData.password;
 
-	authData->hwid = Hardware::getHardwareId();
-	if (authData->hwid.empty()) {
+	authData.hwid = Hardware::getHardwareId();
+	if (authData.hwid.empty()) {
 		throw std::runtime_error(xorstr_("Failed to get hardware id"));
 	}
 
-	authData->token = api->getSessionToken();
-	if (authData->token.empty()) {
+	authData.token = api.getSessionToken();
+	if (authData.token.empty()) {
 		throw std::runtime_error(xorstr_("Failed to get session token"));
 	}
 
-	const std::string convertedAuthData = api->convertAuthDataToJson();
+	const std::string convertedAuthData = api.convertAuthDataToJson();
 	if (convertedAuthData.empty()) {
 		throw std::runtime_error(xorstr_("Failed to get auth data"));
 	}
