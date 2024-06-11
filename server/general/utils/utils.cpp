ad52ad6ad52d7917923b7524e17fc9b6205e822d
soamane <soamane@mail.ru>
2024-06-11 20:11:41 +0300

    Реализована система уникальной обфускации кода программы

--- server/general/utils/utils.cpp
+++ server/general/utils/utils.cpp
@@ -1,43 +1,59 @@
 ﻿#include "utils.h"
 
+#include <windows.h>
 #include <fstream>
-
+#include <iostream>
 #include "../logsystem/logmanager/logmanager.h"
 
-const std::vector<char>& Utils::ConvertFileToBytes(const std::string& path) {
-	if (path.empty()) {
-		throw std::invalid_argument("Function call error: empty argument [" + std::string(__func__) + "]");
-	}
+const std::vector<char> Utils::ConvertFileToBytes(const std::string& path) {
+    if (path.empty()) {
+        throw std::invalid_argument("Function call error: empty argument [" + std::string(__func__) + "]");
+    }
 
-	static std::vector<char> bytes;
-	if (!bytes.empty()) {
-		return bytes;
-	}
+    std::ifstream file(path, std::ios::binary);
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open target file");
+    }
 
-	std::ifstream file(path, std::ios::binary);
-	if (!file.is_open()) {
-		throw std::runtime_error("Failed to open target file");
-	}
+    std::vector<char> bytes((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    if (bytes.empty()) {
+        throw std::runtime_error("Failed to read file information");
+    }
 
-	bytes = std::vector<char>((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-	if (bytes.empty()) {
-		throw std::runtime_error("Failed to read file information");
-	}
-
-	file.close();
-	return bytes;
+    file.close();
+    return bytes;
 }
 
 void Utils::CreateFileFromBytes(const std::string& path, const std::vector<char>& bytes) {
-	if (path.empty() || bytes.empty()) {
-		throw std::invalid_argument("Function call error: empty argument [" + std::string(__func__) + "]");
-	}
+    if (path.empty() || bytes.empty()) {
+        throw std::invalid_argument("Function call error: empty argument [" + std::string(__func__) + "]");
+    }
 
-	std::ofstream file(path, std::ios::binary);
-	if (!file.is_open()) {
-		throw std::runtime_error("Failed to open target file");
-	}
+    std::ofstream file(path, std::ios::binary);
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to open target file");
+    }
 
-	file.write(bytes.data(), bytes.size());
-	file.close();
+    file.write(bytes.data(), bytes.size());
+    file.close();
+}
+
+void Utils::ExecuteObfuscation(const std::string& fileName) {
+    STARTUPINFOA si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    const std::string command = "VMProtect.exe aceid.exe " + fileName + " -pf script.vmp";
+
+    if (!CreateProcessA(NULL, const_cast<LPSTR>(command.c_str()), NULL, NULL, FALSE, CREATE_NEW_CONSOLE | CREATE_NEW_PROCESS_GROUP, NULL, NULL, &si, &pi)) {
+        throw std::runtime_error("Failed to create process");
+    }
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
 }
